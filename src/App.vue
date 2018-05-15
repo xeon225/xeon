b1df1d918111452b67f18d4c4b47b31be24ac1b8
王磊 <11169514@qq.com>
2018-05-15 18:00:54 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="content flex-container top theme_1">
+      <div class="logo bg-white">
+        <img src="./assets/logo.png" width="100%" alt="">
+        <div class="paddingv20 text-center borderb">
+          <div class="fs-20 text-black">
+            name
+          </div>
+          <div class="fs-16">
+            Front End
+          </div>
+        </div>
+        <div class="logo_icon flex-container fs-14 center paddingv20">
+          <div class="iconList border round lh-32 text-black marginh10"><i class="icon-iPhone"></i></div>
+          <div class="iconList border round lh-32 text-black marginh10"><i class="icon-weixin"></i></div>
+          <div class="iconList border round lh-32 text-black marginh10"><i class="icon-qq"></i></div>
+          <div class="iconList border round lh-32 text-black marginh10"><i class="icon-email"></i></div>
+        </div>
+      </div>
+      <div class="article flex1 marginl30">
+        <div class="nav bg-white">
+          123123
+        </div>
+      </div>
+    </div>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #949494;
+}
+</style>
